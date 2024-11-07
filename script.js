53db411354a1c5a70cb1062e6feda226a59e75b6
Irina Zelenova <sorriso19@mail.ru>
2024-11-07 13:35:56 +0300

    выполнена вторая домашка

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let FullPrice;
-let adaptive;
+let title = "Мой первый веб-сайт";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 10000;
+let rollback = 5;
+let fullPrice = 70000;
+let adaptive = true;
 
-alert('Hello, World!');
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
 
-console.log('My first homework');
+
+
+console.log(screens.length);
+let str1 = 'Стоимость верстки экранов';
+let str2 = ' руб.';
+console.log(str1  + " " + screenPrice + str2);
+
+let str3 = 'Стоимость разработки сайта ';
+let str4 = ' руб.';
+console.log(str3  + " " + fullPrice + str4);
+
+console.log(screens.toLowerCase().split(","));
+console.log(fullPrice * (rollback/100));screenPrice
