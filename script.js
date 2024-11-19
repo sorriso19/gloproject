db36bf8d075238669252d79ac17d4c8878838482
Irina Zelenova <sorriso19@mail.ru>
2024-11-19 22:41:03 +0300

    выполнено 5 домашнее задание

--- script.js
+++ script.js
@@ -1,10 +1,93 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let FullPrice;
-let adaptive;
+"use strict";
 
-alert('Hello, World!');
+const rollback = 5
+let title
+let screens
+let screenPrice
+let adaptive
+let allServicePrices
+let fullPrice
+let servicePercentPrice
+let service1
+let service2
 
-console.log('My first homework');
+const isNumber = function(num) {
+    return !isNaN(parseFloat(num) && isFinite(num))
+}
+
+const asking = function () {
+    title = prompt("Как называется ваш проект?", "Калькулятор верстки")
+    screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные")
+    screenPrice = prompt("Сколько будет стоить данная работа?")
+
+    do {
+        screenPrice = prompt("Сколько будет стоить данная работа?");
+        } while (!isNumber(screenPrice));
+      
+    adaptive = confirm("Нужен ли адаптив на сайте?")
+}
+
+const getAllServicePrices = function (num) {
+    let sum = 0
+
+    for (let i = 0; i < 2; i++) {
+        if (i === 0) {
+            service1 = prompt("Какой дополнительный тип услуги нужен?")
+        } else if (i === 1) {
+            service2 = prompt("Какой дополнительный тип услуги нужен?")
+        } else if (!isNaN(parseFloat(num) && isFinite(num))) {
+        sum += +prompt("Сколько это будет стоить?")
+        } 
+    }
+
+    return sum
+}
+
+const showTypeOf = function (variable) {
+    console.log(variable, typeof variable);
+}
+
+const getFullPrice = function () {
+    return screenPrice + allServicePrices;
+}
+
+const getServicePercentPrice = function () {
+    return fullPrice - (fullPrice * (rollback / 100));
+}
+
+const getTitle = function () {
+    return title[0].toUpperCase() + title.substring(1, title.length).toLocaleLowerCase()
+}
+
+const getRollbackMessage = function (fullPrice) {
+    if (fullPrice >= 30000) {
+        return "Даем скидку в 10%";
+    } else if (fullPrice >= 15000 && fullPrice < 30000) {
+        return "Даем скидку в 5%";
+    } else if (fullPrice >= 0 && fullPrice < 15000) {
+        return "Скидка не предусмотрена";
+    } else {
+        return "Ошибка";
+    }
+}
+
+asking()
+allServicePrices = getAllServicePrices()
+fullPrice = getFullPrice()
+servicePercentPrice = getServicePercentPrice()
+title = getTitle()
+
+showTypeOf(title)
+showTypeOf(screenPrice)
+showTypeOf(adaptive)
+
+console.log(getRollbackMessage(fullPrice));
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log("Стоимость верстки экранов: " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта: " + fullPrice + " рублей");
+
+
