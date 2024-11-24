43a7816aa6062f2704cc45c6585b6832bb6465e4
Irina Zelenova <sorriso19@mail.ru>
2024-11-24 16:42:35 +0300

    выполнено д/з 6

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let FullPrice;
-let adaptive;
+'use strict';
 
-alert('Hello, World!');
 
-console.log('My first homework');
+function guessNumber(myNum) {
+    
+    let userNum = +prompt('Угадай число от 1 до 100');
+    
+    return function userNumber() {
+        
+        if (isNaN(parseFloat(userNum) && isFinite(userNum))) {
+            alert ('Введи число!');
+            userNum = +prompt('Введите новое число');
+        }  else if (userNum > myNum) {
+            alert ('Загаданное число меньше');
+          userNum = +prompt('Введите новое число');
+                      
+        } else if (userNum < myNum) {
+            alert ('Загаданное число больше');
+            userNum = +prompt('Введите новое число');
+          
+        } else if (userNum === myNum) {
+            alert ('Поздравляю, Вы угадали!');   
+            
+        } else if (userNum === null) {
+            return confirm('Игра закончена');
+        
+    }  
+
+    userNumber()
+    }
+         
+}                   
+
+        guessNumber(10);
