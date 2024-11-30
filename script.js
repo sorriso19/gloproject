664d034ef0ffeb061e4701f8fe0055df6588a0fb
Irina Zelenova <sorriso19@mail.ru>
2024-11-30 23:06:47 +0300

    функция asking сделана методом appData

--- script.js
+++ script.js
@@ -1,10 +1,84 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let FullPrice;
-let adaptive;
+"use strict";
 
-alert('Hello, World!');
+const appData = {
+    title: '',
+    screens: '',
+    screenPrice: 0 ,
+    adaptive: true,
+    rollback: 5,
+    allServicePrices: 0,
+    fullPrice: 0 ,
+    servicePercentPrice: 0 ,
+    service1: '',
+    service2: '',
+    asking: function () {
+        appData.title = prompt("Как называется ваш проект?", "Калькулятор верстки")
+        appData.screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные")
+           
+        do {
+            appData.screenPrice = prompt("Сколько будет стоить данная работа?");
+            } while (!isNumber(appData.screenPrice));
+          
+        appData.adaptive = confirm("Нужен ли адаптив на сайте?")
+    }
+    
+}
 
-console.log('My first homework');
+const isNumber = function(num) {
+    return !isNaN(parseFloat(num)) && isFinite(num)
+}
+
+const getAllServicePrices = function (num) {
+    let sum = 0
+
+    for (let i = 0; i < 2; i++) {
+        let price = 0
+
+        if (i === 0) {
+            appData.service1 = prompt("Какой дополнительный тип услуги нужен?")
+        } else if (i === 1) {
+            appData.service2 = prompt("Какой дополнительный тип услуги нужен?")
+        }
+
+        do {
+            price = prompt("Сколько это будет стоить?")
+        } while (!isNumber(price))
+
+        sum += +price
+        } 
+        return sum
+    }
+ 
+
+const getFullPrice = function () {
+    return +appData.screenPrice + appData.allServicePrices
+}
+
+const getServicePercentPrice = function () {
+    return appData.fullPrice - (appData.fullPrice * (appData.rollback / 100))
+}
+
+const getTitle = function () {
+    return appData.title[0].toUpperCase() + appData.title.trim().substring(1).toLowerCase()
+}
+
+const getRollbackMessage = function (price) {
+    if (price >= 30000) {
+        return "Даем скидку в 10%"
+    } else if (price >= 15000 && price < 30000) {
+        return "Даем скидку в 5%"
+    } else if (price >= 0 && price < 15000) {
+        return "Скидка не предусмотрена"
+    } else {
+        return "Ошибка"
+    }
+}
+
+appData.asking()
+appData.allServicePrices = getAllServicePrices()
+appData.fullPrice = getFullPrice()
+appData.servicePercentPrice = getServicePercentPrice()
+appData.title = getTitle()
+
+console.log(appData.fullPrice);
+console.log(appData.servicePercentPrice);
