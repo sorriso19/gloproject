ebcab8d12f93c60c8db9d941352ef17774fe345b
Irina Zelenova <sorriso19@mail.ru>
2024-12-09 13:37:22 +0300

    изменения заголовка в Книге 3 и добавление элемента li в Книгу 6

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 "use strict";
 
 const body = document.querySelector('.body')
+const books = document.querySelector('.books')
 const elems = document.querySelectorAll('.book')
 const ad = document.querySelector('.adv')
-const lists = document.querySelectorAll('li')
+const lists = document.querySelector('ul')
 const heading = document.querySelectorAll('h2')
-const li = document.querySelector('book > li')
+const book6 = document.querySelectorAll('.book')
+
+const links = document.querySelectorAll('a')
+let bookList, node
+
+
 
 
 
@@ -15,21 +21,14 @@ elems[5].after(elems[2])
 elems[5].before(elems[3])
 
 document.body.style.backgroundImage = "url(./image/adv.jpg)";
- 
+
 ad.remove()
 
-elems[2].classList.add('book6')
+node = bookList[4].querySelector("a")
+node.innerText = node.innerText.replace('Пропопипы', 'Прототипы')
+
 
 const newElem = document.createElement('li')
 newElem.textContent = 'Глава 8: за пределами ES6'
-newElem.classList.add('new1')
-
-
-
-
-
-
-
-
-// .replaceWith('Книга 3. this и Пропотипы Объектов')
-
+elems[2].classList.add('book6')
+bookList[2].querySelector('ul').lastElementChild.insertAdjacentHTML('beforeend', '<li>Глава 8: За пределами ES6</li>')
