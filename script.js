776be389963395daf5e680e29488d2927ca0b05c
Irina Zelenova <sorriso19@mail.ru>
2024-12-09 11:31:47 +0300

    добавление класса к элементу Книга 6

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 "use strict";
+
 const body = document.querySelector('.body')
 const elems = document.querySelectorAll('.book')
 const ad = document.querySelector('.adv')
 const lists = document.querySelectorAll('li')
 const heading = document.querySelectorAll('h2')
+const li = document.querySelector('book > li')
 
 
 
-console.log(elems);
 
 elems[2].before(elems[0])
 elems[5].after(elems[2])
@@ -17,13 +18,15 @@ document.body.style.backgroundImage = "url(./image/adv.jpg)";
  
 ad.remove()
 
+elems[2].classList.add('book6')
+
 const newElem = document.createElement('li')
 newElem.textContent = 'Глава 8: за пределами ES6'
 newElem.classList.add('new1')
-// listItem.insertAdjacentText()
-// .insertAdjacentText('beforebegin', 'Глава 8: за пределами ES6')
-console.log(elems[2]);
-console.log(newElem);
+
+
+
+
 
 
 
