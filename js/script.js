2afed08fa3a22842020b8c791fd57a848e3990ee
Irina Zelenova <sorriso19@mail.ru>
2024-12-11 23:19:48 +0300

    сделано д/з к уроку 9

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 "use strict";
 
 const title = document.getElementsByTagName('h1')
-const resetButton = document.getElementsByClassName('handler_btn')
-const startButton = document.getElementsByClassName('handler_btn')
 const screenButton = document.querySelector('screen-btn')
-const controlsCheckBox1 = document.querySelectorAll('.other-items number')
-const controlsCheckBox2 = document.querySelectorAll('.other-items percent')
-const inputRange = document.querySelector('rollback > range')
-const span = document.querySelector('rollback > range-value')
-const inputTotal = document.getElementsByClassName('total')
-const inputCount = document.getElementsByClassName('total-count')
-const inputCountOther = document.getElementsByClassName('total-count-other')
-const inputFullCount = document.getElementsByClassName('total-full-count')
-const inputRollbackCount = document.getElementsByClassName('total-count-rollback')
+const otherItemsNumber = document.querySelectorAll('.other-items.number')
+const otherItemsPercent = document.querySelectorAll('.other-items.percent')
+
+const inputRange = document.querySelector('.rollback input')
+const inputRangeValue = document.querySelector('.rollback .range-value')
+
+const startButton = document.getElementsByClassName('handler_btn')[0]
+const resetButton = document.getElementsByClassName('handler_btn')[1]
+
+
+const total = document.getElementsByClassName('total-input')[0]
+const inputCount = document.getElementsByClassName('total-input')[1]
+const inputCountOther = document.getElementsByClassName('total-input')[2]
+const inputFullCount = document.getElementsByClassName('total-input')[3]
+const inputRollbackCount = document.getElementsByClassName('total-input')[4]
+
 let screens = document.querySelectorAll('.screen')
